da1381297f2ca513f6ffdc935a0584d1ac565aec
Artem Fedotov <kebabggbab@gmail.com>
2024-09-13 15:01:52 +0700

    Добавьте файлы проекта.

---
+++ JsonMaster/ManipulatorJSON.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace KebabGGbab.Document.JsonMaster
+{
+    public static class ManipulatorJSON
+    {
+        /// <summary>
+        /// Прочитать файл JSON и десериализовать его
+        /// </summary>
+        /// <typeparam name="T">Тип объекта, в который необходимо десериализировать данные JSON-файла</typeparam>
+        /// <param name="path">Путь к JSON-файлу</param>
+        /// <returns>Десериализованный объект, либо default значение для типа, если файл по указанному пути не существует, либо он пуст или произошла какая-либо ошибка </returns>
+        public static T? LoadJSONConfiguration<T>(string path)
+        {
+            string? content = ReadFileContent(path);
+            if (string.IsNullOrEmpty(content))
+            {
+                return default;
+            }
+            try
+            {
+                return JsonSerializer.Deserialize<T>(content);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Сохраняет объект в файл в формате JSON в кодировке UTF-8 
+        /// </summary>
+        /// <typeparam name="T">Тип данных объекта, который будет сериализован в JSON</typeparam>
+        /// <param name="path">Путь к файлу, в который будет сохранен сериализованный объект</param>
+        /// <param name="obj">Объект, который будет сериализован и сохранен в файл</param>
+        /// <param name="fileMode">Необязательный параметр. Указывает, как операционная система должна открыть файл.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="IOException"></exception>
+        public static void SaveJSONConfiguration<T>(string path, T obj, FileMode fileMode = FileMode.Create)
+        {
+            SaveJSONConfiguration(path, obj, Encoding.UTF8, fileMode);
+        }
+        /// <summary>
+        /// Сохраняет объект в файл в формате JSON в указаной кодировке
+        /// </summary>
+        /// <typeparam name="T">Тип данных объекта, который будет сериализован в JSON</typeparam>
+        /// <param name="path">Путь к файлу, в который будет сохранен сериализованный объект</param>
+        /// <param name="obj">Объект, который будет сериализован и сохранен в файл</param>
+        /// <param name="encoding">Кодировка, в которой необходимо выполнить сохранение</param>
+        /// <param name="fileMode">Необязательный параметр. Указывает, как операционная система должна открыть файл.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="IOException"></exception>
+        public static void SaveJSONConfiguration<T>(string path, T obj, Encoding encoding, FileMode fileMode = FileMode.Create)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException(nameof(path), "Путь к файлу не может быть пустым.");
+            }
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Оюъект должен быть инициализирован.");
+            }
+            if (fileMode == FileMode.CreateNew)
+            {
+                if (File.Exists(path))
+                {
+                    throw new IOException($"Файл '{path}' уже существует.");
+                }
+            }
+            using StreamWriter writer = new(File.Open(path, fileMode), encoding);
+            writer.Write(JsonSerializer.Serialize(obj));
+        }
+
+        /// <summary>
+        /// Читает всё содержимое файла 
+        /// </summary>
+        /// <param name="path">Путь к файлу, который необходимо прочитать</param>
+        /// <returns>Содержимое файла или null, если файл не найден</returns>
+        public static string? ReadFileContent(string path)
+        {
+            if (File.Exists(path))
+            {
+                using StreamReader reader = new(File.Open(path, FileMode.Open));
+                return reader.ReadToEnd();
+            }
+            return null;
+        }
+    }
+}
